7dc3bf97956c7b5c10f1e8a56ff4d73fd1085c37
Jordi <jordi8101@gmail.com>
2021-02-20 11:05:32 -0800

    deploy

--- axolotlteamef/settings.py
+++ axolotlteamef/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '1y$yqg#@cpc$-fh1+jt(xtbh90!%@$z++s*@ej!p$5y4*7^9*t'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
